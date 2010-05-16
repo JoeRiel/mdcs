91268ca279b49434d831dae6836dab9356da0c90
Joe Riel <joer@san.rr.com>
2010-05-16 09:15:42 -0700

    * mdb/lisp/mdb.el (mdb): 	Added check of optional prefix; if applied then, with Maple source buffer, 	add read filename to mdb buffer.

--- lisp/mdb.el
+++ lisp/mdb.el
@@ -741,26 +741,52 @@ A new buffer is created if there is no live buffer."
 
 ;;}}}
 
+;;{{{ Miscellaneous functions
+
+(defun mdb-save-modified-buffer ()
+  "Query user to save current buffer, if modified."
+  (if (and (buffer-modified-p)
+           (y-or-n-p "Save buffer? "))
+      (save-buffer)))
+
+;;}}}
+
 ;;{{{ mdb command
 
-(defun mdb ()
-  "Launch a Maple debugger session.  
-If one already exists, then pop to the debugging buffer."
-  (interactive)
+(defun mdb (insert-read)
+  "Launch a Maple debugger session.
+If one already exists, then pop to the debugging buffer.
+If optional prefix is non-nil and original buffer is a Maple source buffer,
+then insert a command that reads the source file into the mdb buffer."
+  (interactive "P")
+  (let ((orig-file (buffer-file-name))
+	(insert-read (and insert-read
+			  (or (eq major-mode 'maplev-mode)
+			      (eq major-mode 'mpldoc-mode)))))
+    (if insert-read (mdb-save-modified-buffer))
+    
+    (if (buffer-live-p mdb-buffer)
+	;; buffer exists; bring up that buffer
+	(progn
+	  (pop-to-buffer mdb-buffer)
+	  (if insert-read
+	      (insert (format "read \"%s\":" orig-file))))
 
-  (if (buffer-live-p mdb-buffer)
-      ;; buffer exists; bring up that buffer
-      (pop-to-buffer mdb-buffer)
+      ;; buffer does not exist
+      ;; pop to mdb buffer and launch maple
+      (pop-to-buffer (setq mdb-buffer (generate-new-buffer "mdb")))
+      (mdb-mode)
+      ;; Generate 'fake' prompt.
+      (insert (concat (propertize mdb-prompt
+				  'face 'mdb-face-prompt
+				  'rear-nonsticky t)))
+      (set-marker mdb-pmark (point))
+      (if insert-read
+	  (insert (format "read \"%s\":" orig-file))))))
 
-    ;; buffer does not exist:
-    ;; launch maple
-    (pop-to-buffer (setq mdb-buffer (generate-new-buffer "mdb")))
-    (mdb-mode)
-    ;; Generate 'fake' prompt.
-    (insert (concat (propertize mdb-prompt
-				'face 'mdb-face-prompt
-				'rear-nonsticky t)))
-    (set-marker mdb-pmark (point))))
+	
+      
+      
 
 ;;}}}
 
