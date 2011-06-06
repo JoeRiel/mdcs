6ff5f43ef39ed83a6e679c63a51d22e402fe6de3
Joe Riel--w764 <jriel@maplesoft.com>
2011-06-05 21:51:08 -0700

    Replace mds-windows-display-dead with a better method

--- lisp/mds-windows.el
+++ lisp/mds-windows.el
@@ -2,12 +2,9 @@
 
 ;; For single-thread debugging, the default window configuration is to
 ;; split the main window horizontally (emacs-wise), with the live
-;; showstat buffer on the left and the output buffer on the left.
-;; When the dead showstat buffer is activated, it will replace the
-;; live buffer until that buffer receives new output.  Those
-;; two will toggle.
-;;
-;; It isn't clear what to do when the user moves/adds windows.  We shall see.
+;; showstat buffer on the left and the output buffer on the right.
+;; When the dead showstat buffer is activate, it vertically splits the
+;; output buffer.
 
 
 ;; avoid compiler warnings
@@ -20,33 +17,36 @@
   "Display the live-showstat buffer and the output buffer of CLIENT.
 The buffers are displayed in side-by-side windows that fill the
 frame, the showstat buffer on the left.  Return nil."
-  (let ((display-buffer-reuse-frames 't)
-	(live-buf (mds--get-client-live-buf client))
-	(out-buf  (mds--get-client-out-buf  client)))
-    ;; display live-buf in one window that fills frame
-    (delete-other-windows (select-window (display-buffer live-buf)))
-    (set-window-buffer (split-window-horizontally) out-buf)))
+  (let ((display-buffer-reuse-frames 't))
+    (delete-other-windows (select-window (display-buffer (mds--get-client-live-buf client))))
+    (set-window-buffer (split-window-horizontally) (mds--get-client-out-buf client))))
 
-
-;; (defun mds-windows-display-dead (client)
-;;   "Display the dead showstat-buffer of CLIENT in the window
-;; also used by the live buffer."
-;;   ;; temporarily set window user options
-;;   (let ((display-buffer-reuse-frames 't)
-;; 	(same-window-buffer-names 
-;; 	 (list (buffer-name (mds--get-client-live-buf client))
-;; 	       (buffer-name (mds--get-client-dead-buf client)))))
-;;     ;; display the buffer
-;;     (display-buffer dead-buf)))
 
 (defun mds-windows-display-dead (client)
-    ;; select the output window
-  (mds-windows-create client)
-  (select-window (display-buffer (mds--get-client-out-buf client)))
-  (set-window-buffer (split-window-vertically)
-		     (mds--get-client-dead-buf client)))
-		     
-    
+  "Display the dead showstat buffer of CLIENT in a window."
+  (let ((dead-buf (mds--get-client-dead-buf client)))
+    (unless (get-buffer-window-list dead-buf)
+      (let ((out-buf (mds--get-client-out-buf client)))
+	(if (not (get-buffer-window-list out-buf))
+	    ;; no visible buffers, use the basic method
+	    (mds-windows-create client))
+	;; split (one of) the visible out-buf windows
+	(let ((win (display-buffer out-buf)))
+	  (select-window win)
+	  (set-window-buffer (split-window-vertically)
+			     dead-buf))))))
+
+;; Not used, but may later	  
+(defun mds-get-tallest-window (windows)
+  "Return the tallest window in the list WINDOWS."
+  (let ((mx 0) ht win)
+    (while windows
+      (setq ht (window-height (car windows)))
+      (if (> ht mx)
+	  (setq mx ht
+		win (car windows)))
+      (windows (cdr windows)))
+    win))
 
 (provide 'mds-windows)
 
