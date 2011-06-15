c96ab5e5c99b3186a56d4bb4a612b5a7b3da7e39
Joe Riel <joe@gauss>
2011-06-15 09:01:33 -0700

    Initial attempt at defining a menu.
    Should showstat and output buffers share a menu?

---
+++ lisp/mds-menus.el
@@ -0,0 +1,30 @@
+(require 'easymenu)
+
+(easy-menu-define mds-mode-menu '(mds-showstat-mode-map mds-output-mode-map)
+  "MDS menus"
+  '("MDS"
+    ("Server Configurion"
+     [ "Allow anonymous login" mds-menu-allow-anon t]
+     [ "Restrict to localhost" mds-menu-restrict-localhost t]
+     "---"
+     [ "TCP Port" mds-menu-set-port t ]
+     "---"
+     [ "Idle timeout" mds-menu-idle-timeout t]
+     [ "Limit clients" mds-menu-limit-clients t]
+     [ "Maximum string length" mds-menu-max-length t]
+     )
+    ("Window Settings"
+     [ "Beep" mds-menu-beep t]
+     [ "Echo input" mds-menu-echo-input t]
+     [ "Truncate lines" mds-menu-truncate-lines t]
+     [ "Compact view"  ]
+     [ "Enable buttons"]
+     )
+    ("Customize"
+     ["Set colors"])
+    "---"
+    ["Find source"  mds-finder t]
+    "---"
+    ["Restart MDS" mds-restart t]
+    ["Quit MDS" mds-quit t]))
+  
