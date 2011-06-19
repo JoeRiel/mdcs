3bd581c2f50dcb586acef81b64eb3a48ae898e5e
Joe Riel--w764 <jriel@maplesoft.com>
2011-06-18 20:14:01 -0700

    Fix typos in menus

--- lisp/mds-menus.el
+++ lisp/mds-menus.el
@@ -3,11 +3,11 @@
 (easy-menu-define mds-mode-menu '(mds-showstat-mode-map mds-output-mode-map)
   "MDS menus"
   '("MDS"
-    ("Server Configurion"
+    ("Server Configuration"
      [ "Allow anonymous login" mds-menu-allow-anon t]
      [ "Restrict to localhost" mds-menu-restrict-localhost t]
      "---"
-     [ "TCP Port" mds-menu-set-port t ]
+     [ "TCP port" mds-menu-set-port t ]
      "---"
      [ "Idle timeout" mds-menu-idle-timeout t]
      [ "Limit clients" mds-menu-limit-clients t]
