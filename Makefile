71bfefe3783098ad521df45efc52826eae09953a
Joe Riel <joe@gauss>
2011-06-28 16:23:48 -0700

    Added option to specify initial configuration of showstat and output windows.
    Added customizations mds-wm-side-by-side and mds-wm-ss-size to control
    the orientation and size.  Updated info pages.  Created a new customization
    group, mds-wm.

--- Makefile
+++ Makefile
@@ -131,7 +131,7 @@ ELFLAGS	= --no-site-file \
 
 ELC = $(EMACS) --batch $(ELFLAGS) --funcall=batch-byte-compile
 
-ELS = mds-re mds-ss mds-out mds-wm mds-login mds-cp mds-client mds-menu mds
+ELS = mds-re mds-ss mds-out mds-wm mds-login mds-cp mds-client mds
 
 LISP_FILES = $(ELS:%=lisp/%.el)
 ELC_FILES = $(LISP_FILES:.el=.elc)
--- lisp/mds-wm.el
+++ lisp/mds-wm.el
@@ -41,6 +41,27 @@
 (declare-function mds-client-out-buf  "mds")
 
 ;;}}}
+
+;;{{{ constants
+
+(defgroup mds-wm nil
+  "Maple Debugger Server Window Manager"
+  :group 'mds)
+
+(defcustom mds-wm-side-by-side nil
+  "If non-nil, the showstat and output windows are displayed 
+side-by-side in a single client view."
+  :type 'boolean
+  :group 'mds-wm)
+
+(defcustom mds-wm-ss-size nil
+  "If non-nil, specifies the size of the showstat window.
+The size is the width of the window when `mds-wm-side-by-side'
+is non-nil, otherwise it is the height.  If nil, then the
+window is set to the same size as the output window."
+  :type 'boolean
+  :group 'mds-wm)
+
 ;;{{{ variables
 
 (defvar mds-frame nil "Frame used by mds")
@@ -60,7 +81,9 @@ The buffers are displayed in side-by-side windows that fill the
 frame, the showstat buffer on the left.  Return the client."
   (select-frame mds-frame)
   (delete-other-windows (select-window (display-buffer (mds-client-live-buf client))))
-  (set-window-buffer (split-window-horizontally) (mds-client-out-buf client))
+  (set-window-buffer
+   (split-window mds-wm-side-by-side mds-wm-ss-size)
+   (mds-client-out-buf client))
   client)
 
 
